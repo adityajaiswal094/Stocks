89f65d2f99e3252b552a07135cc62cdcb6f21284
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2024-02-24 01:31:49 +0530

    Added .env file and remote connection string

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -3,12 +3,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
+  connectionString: connectionString,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_DATABASE,
+  // port: process.env.DB_PORT,
+  // password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
