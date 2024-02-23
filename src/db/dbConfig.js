7486df753c25594463d9fb0d9340916816f2b9e3
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2024-02-24 02:28:17 +0530

    minor change

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -3,15 +3,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.singapore-postgres.render.com/${process.env.DB_DATABASE}`;
+// const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.singapore-postgres.render.com/${process.env.DB_DATABASE}?ssl=true`;
 
 const pool = new Pool({
-  connectionString: connectionString,
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // database: process.env.DB_DATABASE,
-  // port: process.env.DB_PORT,
-  // password: process.env.DB_PASSWORD,
+  // connectionString: connectionString,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
